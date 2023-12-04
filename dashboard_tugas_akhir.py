a7d8dcbb3ec8d9225dc0ae753680cf480075c6a0
garenkosburne <34509834+garenkosburne@users.noreply.github.com>
2023-12-04 11:35:40 +0700

    Update dashboard_tugas_akhir.py

--- dashboard_tugas_akhir.py
+++ dashboard_tugas_akhir.py
@@ -12,7 +12,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
 sns.set(style='dark')
 from google.colab import drive
 drive.mount('/content/drive')
